927a4aec27eab679ac1418638331361d73bf0250
Fayozbek-7073 <newfayozbek1@gmail.com>
2023-12-08 18:35:11 +0500

    dmkfaslkdv

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,12 @@
 import { HStack, Box, Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
-
 const Header = () => {
   return (
-    <HStack paddingX={"20"} paddingY={"4"} justifyContent={"space-between"} >
-      <Box><img width={"50px"} src="/src/img/my/favicon.png" alt="F" /></Box>
+    <HStack paddingX={"20"} paddingY={"4"} justifyContent={"space-between"}>
+      <Box>
+        <img width={"50px"} src="../src/assets/favicon.png" alt="F" />
+      </Box>
       <Box>
         <Link to={"/portfolio"}>
           <Button>My Profile</Button>
